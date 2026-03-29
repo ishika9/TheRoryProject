0cd9c08add74f553d1e003c3b2144e82c28e9db1
Ishika Sunil <ishika9sunil@gmail.com>
2026-03-29 16:14:23 +0530

    upgrading db to postgres on render

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,11 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./rory.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
+
+
+# DATABASE_URL = "sqlite:///./rory.db"
+
+# engine = create_engine(
+#     DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 SessionLocal = sessionmaker(
     bind=engine,
